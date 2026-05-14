b7b1a3419e771290ef1e734c88bf07068ae16432
LEEJH2003 <dl4593@kau.kr>
2026-05-14 20:32:53 +0900

    chore: spring Boot 프로젝트 생성 및 의존성 설정

---
+++ src/test/java/likelion/_th/lte/LteApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion._th.lte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
