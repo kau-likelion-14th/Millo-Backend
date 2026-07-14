2d4985ab7d2c18c26fa9eae7a93f9742940e30aa
LEEJH2003 <dl4593@kau.kr>
2026-07-14 23:15:59 +0900

    fix: cotroller 오타 수정 (controller)

--- src/main/java/likelion14th/lte/follow/controller/FollowController.java
+++ src/main/java/likelion14th/lte/follow/controller/FollowController.java
@@ -1,4 +1,4 @@
-package likelion14th.lte.follow.cotroller;
+package likelion14th.lte.follow.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
